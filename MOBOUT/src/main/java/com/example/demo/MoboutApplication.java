8bcc527791e608b537d086b9bce172c9271f55e2
DigeoG <diegoemilio0225@gmail.com>
2025-07-15 20:26:32 -0500

    new stuff

---
+++ MOBOUT/src/main/java/com/example/demo/MoboutApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoboutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoboutApplication.class, args);
+	}
+
+}
